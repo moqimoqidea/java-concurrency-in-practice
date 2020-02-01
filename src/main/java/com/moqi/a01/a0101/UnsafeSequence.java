943dd4168c269fcb42cd7c19a4d7a96b9fe4807b
moqi <moqimoqidea@gmail.com>
2020-02-01 21:26:38 +0800

    增加两个类 UnsafeSequence.java 和 Sequence.java

---
+++ src/main/java/com/moqi/a01/a0101/UnsafeSequence.java
@@ -0,0 +1,21 @@
+package com.moqi.a01.a0101;
+
+import net.jcip.annotations.NotThreadSafe;
+
+/**
+ * UnsafeSequence
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@NotThreadSafe
+public class UnsafeSequence {
+    private int value;
+
+    /**
+     * Returns a unique value.
+     */
+    public int getNext() {
+        return value++;
+    }
+}
