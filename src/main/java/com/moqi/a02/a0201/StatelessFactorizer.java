d03d9493b6720e366f9ceb54a01ffb6e8fbcbfe5
moqi <moqimoqidea@gmail.com>
2020-02-01 21:35:39 +0800

    pom 增加 javax.servlet-api，增加类 StatelessFactorizer.java

---
+++ src/main/java/com/moqi/a02/a0201/StatelessFactorizer.java
@@ -0,0 +1,36 @@
+package com.moqi.a02.a0201;
+
+import net.jcip.annotations.ThreadSafe;
+
+import javax.servlet.*;
+import java.math.BigInteger;
+
+/**
+ * StatelessFactorizer
+ * <p>
+ * A stateless servlet
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class StatelessFactorizer extends GenericServlet implements Servlet {
+
+    @Override
+    public void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        BigInteger[] factors = factor(i);
+        encodeIntoResponse(resp, factors);
+    }
+
+    void encodeIntoResponse(ServletResponse resp, BigInteger[] factors) {
+    }
+
+    BigInteger extractFromRequest(ServletRequest req) {
+        return new BigInteger("7");
+    }
+
+    BigInteger[] factor(BigInteger i) {
+        // Doesn't really factor
+        return new BigInteger[]{i};
+    }
+}
