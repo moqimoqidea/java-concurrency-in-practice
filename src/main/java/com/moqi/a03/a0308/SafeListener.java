162d0e36ccef61ac2def440d29fa81e6eeb2ae8c
moqi <moqimoqidea@gmail.com>
2020-02-03 09:15:38 +0800

    新增类 SafeListener.java

---
+++ src/main/java/com/moqi/a03/a0308/SafeListener.java
@@ -0,0 +1,43 @@
+package com.moqi.a03.a0308;
+
+/**
+ * SafeListener
+ * <p/>
+ * Using a factory method to prevent the this reference from escaping during construction
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class SafeListener {
+    private final EventListener listener;
+
+    private SafeListener() {
+        // 这里可以替换为 Lambda 表达式：listener = this::doSomething;
+        listener = new EventListener() {
+            @Override
+            public void onEvent(Event e) {
+                doSomething(e);
+            }
+        };
+    }
+
+    public static SafeListener newInstance(EventSource source) {
+        SafeListener safe = new SafeListener();
+        source.registerListener(safe.listener);
+        return safe;
+    }
+
+    void doSomething(Event e) {
+    }
+
+
+    interface EventSource {
+        void registerListener(EventListener e);
+    }
+
+    interface EventListener {
+        void onEvent(Event e);
+    }
+
+    interface Event {
+    }
+}
