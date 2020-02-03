8d7c335b16f9a7e1870fb247aa3d74f9f33474d2
moqi <moqimoqidea@gmail.com>
2020-02-03 09:09:11 +0800

    新增类 ThisEscape.java

---
+++ src/main/java/com/moqi/a03/a0307/ThisEscape.java
@@ -0,0 +1,34 @@
+package com.moqi.a03.a0307;
+
+/**
+ * ThisEscape
+ * <p/>
+ * Implicitly allowing the this reference to escape
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class ThisEscape {
+    public ThisEscape(EventSource source) {
+        // 这里可以替换为 Lambda 表达式：source.registerListener(this::doSomething);
+        source.registerListener(new EventListener() {
+            @Override
+            public void onEvent(Event e) {
+                doSomething(e);
+            }
+        });
+    }
+
+    void doSomething(Event e) {
+    }
+
+    interface EventSource {
+        void registerListener(EventListener e);
+    }
+
+    interface EventListener {
+        void onEvent(Event e);
+    }
+
+    interface Event {
+    }
+}
