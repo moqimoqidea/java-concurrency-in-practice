dffc07183ad76ab810e97a60a52a2a64018e7a8a
moqi <moqimoqidea@gmail.com>
2020-02-04 21:37:22 +0800

    Update NumberRange.java

--- src/main/java/com/moqi/a04/a0410/NumberRange.java
+++ src/main/java/com/moqi/a04/a0410/NumberRange.java
@@ -6,17 +6,18 @@ import java.util.concurrent.atomic.AtomicInteger;
  * NumberRange
  * <p/>
  * Number range class that does not sufficiently protect its invariants
+ * NumberRange 类并不足以保护它的不变性条件（不要这么做）
  *
  * @author Brian Goetz and Tim Peierls
  */
 
 public class NumberRange {
-    // INVARIANT: lower <= upper
+    // INVARIANT（不变性条件）: lower <= upper
     private final AtomicInteger lower = new AtomicInteger(0);
     private final AtomicInteger upper = new AtomicInteger(0);
 
     public void setLower(int i) {
-        // Warning -- unsafe check-then-act
+        // Warning -- unsafe check-then-act：注意——不安全的先检查后执行
         if (i > upper.get()) {
             throw new IllegalArgumentException("can't set lower to " + i + " > upper");
         }
@@ -24,7 +25,7 @@ public class NumberRange {
     }
 
     public void setUpper(int i) {
-        // Warning -- unsafe check-then-act
+        // Warning -- unsafe check-then-act：注意——不安全的先检查后执行
         if (i < lower.get()) {
             throw new IllegalArgumentException("can't set upper to " + i + " < lower");
         }
