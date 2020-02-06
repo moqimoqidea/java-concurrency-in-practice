75fe67ea3b50ccd48a3ee2a16d3b6bac93efed32
moqi <moqimoqidea@gmail.com>
2020-02-06 21:48:12 +0800

    Add Memoizer.java

---
+++ src/main/java/com/moqi/a05/a0519/Memoizer.java
@@ -0,0 +1,52 @@
+package com.moqi.a05.a0519;
+
+import com.moqi.a05.a0513.LaunderThrowable;
+import org.apache.commons.lang3.concurrent.Computable;
+
+import java.util.concurrent.*;
+
+/**
+ * Memoizer
+ * <p/>
+ * Final implementation of Memoizer
+ * Memoizer 的最终实现
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class Memoizer<A, V> implements Computable<A, V> {
+    private final ConcurrentMap<A, Future<V>> cache
+            = new ConcurrentHashMap<A, Future<V>>();
+    private final Computable<A, V> c;
+
+    public Memoizer(Computable<A, V> c) {
+        this.c = c;
+    }
+
+    @Override
+    public V compute(final A arg) throws InterruptedException {
+        while (true) {
+            Future<V> f = cache.get(arg);
+            if (f == null) {
+                Callable<V> eval = new Callable<V>() {
+                    @Override
+                    public V call() throws InterruptedException {
+                        return c.compute(arg);
+                    }
+                };
+                FutureTask<V> ft = new FutureTask<V>(eval);
+                f = cache.putIfAbsent(arg, ft);
+                if (f == null) {
+                    f = ft;
+                    ft.run();
+                }
+            }
+            try {
+                return f.get();
+            } catch (CancellationException e) {
+                cache.remove(arg, f);
+            } catch (ExecutionException e) {
+                throw LaunderThrowable.launderThrowable(e.getCause());
+            }
+        }
+    }
+}
