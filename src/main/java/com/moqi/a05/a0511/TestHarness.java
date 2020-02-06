f8ac7db2416978e4f51c0c7c726865f2b3b43f69
moqi <moqimoqidea@gmail.com>
2020-02-06 20:46:40 +0800

    Add TestHarness.java

---
+++ src/main/java/com/moqi/a05/a0511/TestHarness.java
@@ -0,0 +1,43 @@
+package com.moqi.a05.a0511;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * TestHarness
+ * <p/>
+ * Using CountDownLatch for starting and stopping threads in timing tests
+ * 在计时测试中使用 CountDownLatch 来启动和停止线程
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class TestHarness {
+    public long timeTasks(int nThreads, final Runnable task)
+            throws InterruptedException {
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            Thread t = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        startGate.await();
+                        try {
+                            task.run();
+                        } finally {
+                            endGate.countDown();
+                        }
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            };
+            t.start();
+        }
+
+        long start = System.nanoTime();
+        startGate.countDown();
+        endGate.await();
+        long end = System.nanoTime();
+        return end - start;
+    }
+}
