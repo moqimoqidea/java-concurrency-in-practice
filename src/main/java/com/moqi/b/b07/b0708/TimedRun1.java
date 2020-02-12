5e6b0cc407a54176520b4ac35ab2f5ba69939ff0
moqi <moqimoqidea@gmail.com>
2020-02-12 21:58:11 +0800

    Add TimedRun1.java

---
+++ src/main/java/com/moqi/b/b07/b0708/TimedRun1.java
@@ -0,0 +1,28 @@
+package com.moqi.b.b07.b0708;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * InterruptBorrowedThread
+ * <p/>
+ * Scheduling an interrupt on a borrowed thread
+ * 在外部线程中安排中断（不要这么做）
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class TimedRun1 {
+    private static final ScheduledExecutorService cancelExec = Executors.newScheduledThreadPool(1);
+
+    public static void timedRun(Runnable r,
+                                long timeout, TimeUnit unit) {
+        final Thread taskThread = Thread.currentThread();
+        cancelExec.schedule(new Runnable() {
+            public void run() {
+                taskThread.interrupt();
+            }
+        }, timeout, unit);
+        r.run();
+    }
+}
