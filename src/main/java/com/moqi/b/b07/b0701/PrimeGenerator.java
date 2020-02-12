17db6f5a1e066426900830ed4fe75b4dacb28ba7
moqi <moqimoqidea@gmail.com>
2020-02-12 17:53:57 +0800

    Add PrimeGenerator.java

---
+++ src/main/java/com/moqi/b/b07/b0701/PrimeGenerator.java
@@ -0,0 +1,59 @@
+package com.moqi.b.b07.b0701;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * PrimeGenerator
+ * <p/>
+ * Using a volatile field to hold cancellation state
+ * 使用 volatile 类型的域来保存取消状态
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class PrimeGenerator implements Runnable {
+    private static final ExecutorService EXEC = Executors.newCachedThreadPool();
+
+    @GuardedBy("this")
+    private final List<BigInteger> primes
+            = new ArrayList<BigInteger>();
+    private volatile boolean cancelled;
+
+    static List<BigInteger> aSecondOfPrimes() throws InterruptedException {
+        PrimeGenerator generator = new PrimeGenerator();
+        EXEC.execute(generator);
+        try {
+            SECONDS.sleep(1);
+        } finally {
+            generator.cancel();
+        }
+        return generator.get();
+    }
+
+    public void run() {
+        BigInteger p = BigInteger.ONE;
+        while (!cancelled) {
+            p = p.nextProbablePrime();
+            synchronized (this) {
+                primes.add(p);
+            }
+        }
+    }
+
+    public void cancel() {
+        cancelled = true;
+    }
+
+    public synchronized List<BigInteger> get() {
+        return new ArrayList<BigInteger>(primes);
+    }
+}
