c4a78d299d8da3712535c13d8bd82240c4ba868e
moqi <moqimoqidea@gmail.com>
2020-02-12 16:42:13 +0800

    Add FutureRenderer.java

---
+++ src/main/java/com/moqi/b/b06/b0613/FutureRenderer.java
@@ -0,0 +1,62 @@
+package com.moqi.b.b06.b0613;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import static com.moqi.a.a05.a0513.LaunderThrowable.launderThrowable;
+
+
+/**
+ * FutureRenderer
+ * <p/>
+ * Waiting for image download with Future
+ * 使用 Future 等待图像下载
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public abstract class FutureRenderer {
+    private final ExecutorService executor = Executors.newCachedThreadPool();
+
+    void renderPage(CharSequence source) {
+        final List<ImageInfo> imageInfos = scanForImageInfo(source);
+        Callable<List<ImageData>> task =
+                new Callable<List<ImageData>>() {
+                    public List<ImageData> call() {
+                        List<ImageData> result = new ArrayList<ImageData>();
+                        for (ImageInfo imageInfo : imageInfos)
+                            result.add(imageInfo.downloadImage());
+                        return result;
+                    }
+                };
+
+        Future<List<ImageData>> future = executor.submit(task);
+        renderText(source);
+
+        try {
+            List<ImageData> imageData = future.get();
+            for (ImageData data : imageData)
+                renderImage(data);
+        } catch (InterruptedException e) {
+            // Re-assert the thread's interrupted status
+            Thread.currentThread().interrupt();
+            // We don't need the result, so cancel the task too
+            future.cancel(true);
+        } catch (ExecutionException e) {
+            throw launderThrowable(e.getCause());
+        }
+    }
+
+    abstract void renderText(CharSequence s);
+
+    abstract List<ImageInfo> scanForImageInfo(CharSequence s);
+
+    abstract void renderImage(ImageData i);
+
+    interface ImageData {
+    }
+
+    interface ImageInfo {
+        ImageData downloadImage();
+    }
+}
