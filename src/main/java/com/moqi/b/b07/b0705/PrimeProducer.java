e3a8b270a3fecbd3183d31de430aa355aa3836a4
moqi <moqimoqidea@gmail.com>
2020-02-12 18:21:00 +0800

    Add PrimeProducer.java

---
+++ src/main/java/com/moqi/b/b07/b0705/PrimeProducer.java
@@ -0,0 +1,34 @@
+package com.moqi.b.b07.b0705;
+
+import java.math.BigInteger;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * PrimeProducer
+ * <p/>
+ * Using interruption for cancellation
+ * 通过中断来取消
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class PrimeProducer extends Thread {
+    private final BlockingQueue<BigInteger> queue;
+
+    PrimeProducer(BlockingQueue<BigInteger> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        try {
+            BigInteger p = BigInteger.ONE;
+            while (!Thread.currentThread().isInterrupted())
+                queue.put(p = p.nextProbablePrime());
+        } catch (InterruptedException consumed) {
+            /* Allow thread to exit */
+        }
+    }
+
+    public void cancel() {
+        interrupt();
+    }
+}
