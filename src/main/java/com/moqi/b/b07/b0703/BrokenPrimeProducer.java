c5ceb13a9c94734514884ee3e1bb05e798bc73c6
moqi <moqimoqidea@gmail.com>
2020-02-12 18:12:39 +0800

    Add BrokenPrimeProducer.java

---
+++ src/main/java/com/moqi/b/b07/b0703/BrokenPrimeProducer.java
@@ -0,0 +1,34 @@
+package com.moqi.b.b07.b0703;
+
+import java.math.BigInteger;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * BrokenPrimeProducer
+ * <p/>
+ * Unreliable cancellation that can leave producers stuck in a blocking operation
+ * 不可靠的取消操作将把生产者置于阻塞的操作中（不要这么做）
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+class BrokenPrimeProducer extends Thread {
+    private final BlockingQueue<BigInteger> queue;
+    private volatile boolean cancelled = false;
+
+    BrokenPrimeProducer(BlockingQueue<BigInteger> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        try {
+            BigInteger p = BigInteger.ONE;
+            while (!cancelled)
+                queue.put(p = p.nextProbablePrime());
+        } catch (InterruptedException consumed) {
+        }
+    }
+
+    public void cancel() {
+        cancelled = true;
+    }
+}
