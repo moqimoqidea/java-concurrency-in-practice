241da76e4d4e76e5bd9d74d2c572f8e1079a7e87
moqi <moqimoqidea@gmail.com>
2020-02-12 18:46:15 +0800

    Add NoncancelableTask.java

---
+++ src/main/java/com/moqi/b/b07/b0707/NoncancelableTask.java
@@ -0,0 +1,33 @@
+package com.moqi.b.b07.b0707;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * NoncancelableTask
+ * <p/>
+ * Noncancelable task that restores interruption before exit
+ * 不可取消的任务在退出前恢复中断
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class NoncancelableTask {
+    public Task getNextTask(BlockingQueue<Task> queue) {
+        boolean interrupted = false;
+        try {
+            while (true) {
+                try {
+                    return queue.take();
+                } catch (InterruptedException e) {
+                    interrupted = true;
+                    // fall through and retry
+                }
+            }
+        } finally {
+            if (interrupted)
+                Thread.currentThread().interrupt();
+        }
+    }
+
+    interface Task {
+    }
+}
