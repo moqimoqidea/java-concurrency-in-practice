86206fa4c5199a61ec4a861bce0fd93260b00bc4
moqi <moqimoqidea@gmail.com>
2020-02-12 15:57:31 +0800

    Add SingleThreadWebServer.java

---
+++ src/main/java/com/moqi/b/b01/b0601/SingleThreadWebServer.java
@@ -0,0 +1,28 @@
+package com.moqi.b.b01.b0601;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * SingleThreadWebServer
+ * <p/>
+ * Sequential web server
+ * 串行的 Web 服务器
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+public class SingleThreadWebServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket socket = new ServerSocket(80);
+        while (true) {
+            Socket connection = socket.accept();
+            handleRequest(connection);
+        }
+    }
+
+    private static void handleRequest(Socket connection) {
+        // request-handling logic here
+    }
+}
