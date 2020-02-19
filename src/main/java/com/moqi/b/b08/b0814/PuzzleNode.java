483f65469c4c256ce848e67aa56800e9e3a61199
moqi <moqimoqidea@gmail.com>
2020-02-19 19:51:58 +0800

    Add PuzzleNode.java

---
+++ src/main/java/com/moqi/b/b08/b0814/PuzzleNode.java
@@ -0,0 +1,34 @@
+package com.moqi.b.b08.b0814;
+
+import net.jcip.annotations.Immutable;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * PuzzleNode
+ * <p/>
+ * Link node for the puzzle solving framework
+ * 用于谜题解决框架的链表节点
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+@Immutable
+public class PuzzleNode <P, M> {
+    final P pos;
+    final M move;
+    final PuzzleNode<P, M> prev;
+
+    public PuzzleNode(P pos, M move, PuzzleNode<P, M> prev) {
+        this.pos = pos;
+        this.move = move;
+        this.prev = prev;
+    }
+
+    List<M> asMoveList() {
+        List<M> solution = new LinkedList<M>();
+        for (PuzzleNode<P, M> n = this; n.move != null; n = n.prev)
+            solution.add(0, n.move);
+        return solution;
+    }
+}
