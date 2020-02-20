8703aa0235fd49af9428fb20ace2e3e648e1d1b0
moqi <moqimoqidea@gmail.com>
2020-02-20 21:10:03 +0800

    Add BetterAttributeStore.java

---
+++ src/main/java/com/moqi/c/c11/c1105/BetterAttributeStore.java
@@ -0,0 +1,35 @@
+package com.moqi.c.c11.c1105;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+/**
+ * BetterAttributeStore
+ * <p/>
+ * Reducing lock duration
+ * 减少锁的持有时间
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class BetterAttributeStore {
+    @GuardedBy("this")
+    private final Map<String, String>
+            attributes = new HashMap<String, String>();
+
+    public boolean userLocationMatches(String name, String regexp) {
+        String key = "users." + name + ".location";
+        String location;
+        synchronized (this) {
+            location = attributes.get(key);
+        }
+        if (location == null)
+            return false;
+        else
+            return Pattern.matches(regexp, location);
+    }
+}
