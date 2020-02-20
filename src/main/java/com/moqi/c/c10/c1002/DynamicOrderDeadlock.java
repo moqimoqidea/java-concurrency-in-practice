70987c525e5d4a534821034f378bd8d9c7155ebd
moqi <moqimoqidea@gmail.com>
2020-02-20 20:12:35 +0800

    Add DynamicOrderDeadlock.java

---
+++ src/main/java/com/moqi/c/c10/c1002/DynamicOrderDeadlock.java
@@ -0,0 +1,78 @@
+package com.moqi.c.c10.c1002;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * DynamicOrderDeadlock
+ * <p/>
+ * Dynamic lock-ordering deadlock
+ * 动态的锁顺序死锁（不要这么做）
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class DynamicOrderDeadlock {
+    // Warning: deadlock-prone!
+    public static void transferMoney(Account fromAccount,
+                                     Account toAccount,
+                                     DollarAmount amount)
+            throws InsufficientFundsException {
+        synchronized (fromAccount) {
+            synchronized (toAccount) {
+                if (fromAccount.getBalance().compareTo(amount) < 0)
+                    throw new InsufficientFundsException();
+                else {
+                    fromAccount.debit(amount);
+                    toAccount.credit(amount);
+                }
+            }
+        }
+    }
+
+    static class DollarAmount implements Comparable<DollarAmount> {
+        // Needs implementation
+
+        public DollarAmount(int amount) {
+        }
+
+        public DollarAmount add(DollarAmount d) {
+            return null;
+        }
+
+        public DollarAmount subtract(DollarAmount d) {
+            return null;
+        }
+
+        public int compareTo(DollarAmount dollarAmount) {
+            return 0;
+        }
+    }
+
+    static class Account {
+        private static final AtomicInteger sequence = new AtomicInteger();
+        private final int acctNo;
+        private DollarAmount balance;
+
+        public Account() {
+            acctNo = sequence.incrementAndGet();
+        }
+
+        void debit(DollarAmount d) {
+            balance = balance.subtract(d);
+        }
+
+        void credit(DollarAmount d) {
+            balance = balance.add(d);
+        }
+
+        DollarAmount getBalance() {
+            return balance;
+        }
+
+        int getAcctNo() {
+            return acctNo;
+        }
+    }
+
+    static class InsufficientFundsException extends Exception {
+    }
+}
